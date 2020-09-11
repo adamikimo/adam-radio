afb052b56ac4b5917eb13876752d6b1bdf0be73c
adamikimo <68379640+adamikimo@users.noreply.github.com>
2020-09-11 20:57:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ var boolean = true;
 var playPause = document.querySelector('.playPause');
 var audioTitle = document.querySelector('.audio-title');
 var liveRadioNames = [];
-liveRadioNames[0] = "RADIO EGYPT";
-liveRadioNames[1] = "NILE FM";
-liveRadioNames[2] = "ALJAZAIR RADIO";
+liveRadioNames[0] = "NILE FM";
+liveRadioNames[1] = "ALJAZAIR RADIO";
 
 var liveRadioStations = [];
-liveRadioStations[0] = "http://178.32.62.154:8957/;stream.mp3";
-liveRadioStations[1] = "https://ice1.securenetsystems.net/DEMOSTN?playSessionID=A7F278BC-EBCC-9B91-A947A1C7020EE682";
-liveRadioStations[2] = "https://chlef.ice.infomaniak.ch/02.aac";
+liveRadioStations[0] = "https://ice1.securenetsystems.net/DEMOSTN?playSessionID=A7F278BC-EBCC-9B91-A947A1C7020EE682";
+liveRadioStations[1] = "https://chlef.ice.infomaniak.ch/02.aac";
 
 playPause.addEventListener("click" , ()=>{
 if(boolean == true){
